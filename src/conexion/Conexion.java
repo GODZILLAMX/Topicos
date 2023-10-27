0e412965fd78be79211cf6b3b53f4917faac6483
GODZILLAMX <martin.eduar2345@gmail.com>
2023-10-27 15:38:38 -0600

    Carga inicial

---
+++ src/conexion/Conexion.java
@@ -0,0 +1,38 @@
+
+package conexion;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    Connection con;
+    Statement stmt;
+    ResultSet rs;
+    public Conexion(){
+        try
+        {
+            //Class.forName("com.mysql.jdbc.Driver");
+            
+            String url ="jdbc:mysql://localhost:3306/mastografica";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mastografica","root","Agbaes31@_");     
+            //stmt = con.createStatement();
+            //stmt.executeUpdate("INSERT INTO clientes VALUES(1,'CARLOS','SANCHEZ',40)" );
+//            JOptionPane.showMessageDialog(null,"Conexión establecida");
+        }
+        catch(Exception ex)
+        //catch(ClassNotFoundException ex)
+        {
+            //Logger.getLogger(conexion.class.getName()).log(Level.SEVERE,null,ex);
+            JOptionPane.showMessageDialog(null,"No se pudo establecer la conexion . Error+"+ex);
+        }
+            
+    }
+    public Connection getConnection()
+    {
+        return con;
+    }
+}
